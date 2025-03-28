41ce01b62b21806d96812118636ac606faea5f58
Ardit <ardit@starlabspro.com>
2025-03-28 15:30:01 +0100

    Update gpt-response.blade.php

--- resources/views/filament/resources/h5p/gpt-response.blade.php
+++ resources/views/filament/resources/h5p/gpt-response.blade.php
@@ -26,12 +26,17 @@
                             <div class="flex items-center space-x-2">
                                 @if (isset($question['correct']) && $question['correct'] === $ansIndex)
                                     <span class="text-success-500">
-                                        <x-heroicon-s-check-circle class="w-5 h-5" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                                        </svg>
                                     </span>
                                     <span class="text-success-600 font-medium">{{ $answer }}</span>
                                 @else
                                     <span class="text-gray-400">
-                                        <x-heroicon-o-circle class="w-5 h-5" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 9.75L15 15m0-5.25L9 15" />
+                                        </svg>
                                     </span>
                                     <span class="text-gray-600">{{ $answer }}</span>
                                 @endif
