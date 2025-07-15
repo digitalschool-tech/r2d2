5bf9388095f6a5c8fbfce530496b94f9bf6a6128
puhize <puhizarexha@hotmail.com>
2025-07-15 11:14:06 +0200

    create quizzes table

---
+++ database/migrations/2025_07_15_091042_create_quizzes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('question'); // Question text
+            $table->json('answers'); // Answers stored as JSON
+            $table->string('correct'); // Correct answer, can be a string or an ID
+            $table->string('sub_content_id'); // Foreign key to the sub_content
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
