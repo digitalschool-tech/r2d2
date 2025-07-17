f0e5f7eafb9aa5009b609caf208a8f1312e17249
Ardit <arditxhaferi2@gmail.com>
2025-07-17 13:29:57 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,11 @@
 <?php
 
 use App\Http\Controllers\AudioController;
-use App\Http\Controllers\MoodleController;
 use App\Http\Controllers\CurriculumController;
 use App\Http\Controllers\CurriculumExportController;
 use App\Http\Controllers\QuizController;
 
 Route::post('/generate-audio', [AudioController::class, 'generate']);
-
-// Restricted route with CORS middleware
-Route::middleware(['cors.custom'])->group(function () {
-    // Route::post('/generate-quiz', [MoodleController::class, 'generateH5PAndUpload']);
-    Route::post('/generate-quiz', [QuizController::class, 'generateQuizFromCurriculum']);
-});
 Route::post('/generate-quiz', [QuizController::class, 'generateQuizFromCurriculum']);
 Route::post('/curriculum', [CurriculumController::class, 'store']);
 Route::get('/export-lessons', [CurriculumExportController::class, 'exportCsv']);
